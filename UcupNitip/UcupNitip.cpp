183295b95b2be1ebac06bb2853de9f4763d008e0
CaptainAAI <anandaagungismail@gmail.com>
2024-03-08 11:47:34 +0700

    prosedur untuk mengurutkan array dengan metode bubble sort

--- UcupNitip/UcupNitip.cpp
+++ UcupNitip/UcupNitip.cpp
@@ -25,3 +25,18 @@ void input() {
 		cin >> a[i];
 	}
 }
+
+void bubblesortarray() {
+	int pass = 1;
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) {
+			if (a[j] > a[j + 1]) {
+				int temp;
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+		pass = pass + 1;
+	} while (pass <= n - 1);
+}
