5f87f9d96dfb411b16400628ecd575329731465f
CaptainAAI <anandaagungismail@gmail.com>
2024-03-08 11:43:55 +0700

    prosedur input

--- UcupNitip/UcupNitip.cpp
+++ UcupNitip/UcupNitip.cpp
@@ -4,3 +4,24 @@ using namespace std;
 
 int a[20];
 int n;
+
+void input() {
+	while (true) {
+		cout << "Masukkan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+
+		}
+	}
+	cout << endl;
+	cout << "=================" << endl;
+	cout << "Masukkan elemen array" << endl;
+	cout << "=================" << endl;
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ":";
+		cin >> a[i];
+	}
+}
