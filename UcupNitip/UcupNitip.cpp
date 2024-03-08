bda92d0e38e3c0879cb7f52e2c3441d204a5f569
CaptainAAI <anandaagungismail@gmail.com>
2024-03-08 11:50:26 +0700

    function display

--- UcupNitip/UcupNitip.cpp
+++ UcupNitip/UcupNitip.cpp
@@ -40,3 +40,15 @@ void bubblesortarray() {
 		pass = pass + 1;
 	} while (pass <= n - 1);
 }
+
+void display() {
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "======================" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << a[j] << endl;
+	}
+	cout << "Jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
